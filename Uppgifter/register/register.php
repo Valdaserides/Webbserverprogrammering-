dad451da18e142236f0df5cb2eb6dca08da78db7
Valdaserides <max.valdaseridesolofsson.te16c@gmail.com>
2017-11-24 09:59:11 +0100

    lade till register.php i mappen register

---
+++ Uppgifter/register/register.php
@@ -0,0 +1,71 @@
+<style>
+.formulär{
+	
+	clear: both;
+	width: 300px;
+	text-align: center;
+	
+}
+.formulär input{
+	
+	clear: both;
+	width: 100%;
+	
+}
+
+</style>
+<div class="formulär">
+
+	<form method="POST" action="submit.php">
+
+		Användarnamn: <input type="text" name="username">
+		<br>
+		Namn: <input type="text" name="name">
+		<br>
+		E-mail: <input type="text" name="email">
+		<br>
+		Födelsedatum (YYYYMMDD): <input type="text" name="date">
+		<br>
+		Lösenord: <input type="password" name="pwd">
+		<br>
+		<input type="submit" name="submit">
+
+	</form>
+	
+</div>
+	
+<?php
+
+	
+	
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

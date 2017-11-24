1e9cd3b208eb21689e0c9d7166f0299f44bfc309
Valdaserides <max.valdaseridesolofsson.te16c@gmail.com>
2017-11-24 10:09:48 +0100

    la in databasen i login mappen

---
+++ Uppgifter/login/sql.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE register;
+
+CREATE TABLE data(id int(8) AUTO_INCREMENT PRIMARY KEY, användarnamn varchar(100), namn varchar(100), email varchar(100), date varchar(100), password varchar(100));
+

eb7cc4e2329a32044d01f8adad24dcbe0c835346
Valdaserides <max.valdaseridesolofsson.te16c@gmail.com>
2017-11-30 14:34:26 +0100

    gjorde en connction till databasen

--- Uppgifter/login/submit.php
+++ Uppgifter/login/submit.php
@@ -4,23 +4,7 @@
 	$user = "root";
 	$password = "";
 	$database = "register";
-
+	
 	$dbc = mysqli_connect($adress,$user,$password,$database);
-
-	$username = $_POST['username'];
-	$password = $_POST['pwd'];
-	
-	$insert = "INSERT INTO data (användarnamn, namn, email, date, password) VALUES ('$username', '$fullName', '$email', '$date', '$password');";
-	
-	if(mysqli_query($dbc, $insert)){
-		echo "Success!";
-	}
-	else{
-		echo mysqli_error($dbc);
-		echo '<br>';
-		echo "Error!";
-	}
-	
-	
 
 ?>

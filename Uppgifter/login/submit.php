3ab5ee028fd86aa6715c0c5acbf67844cda63679
Valdaserides <max.valdaseridesolofsson.te16c@gmail.com>
2017-11-30 14:42:17 +0100

    gjorde en MYSQL fråga som hämtar information från databasen

--- Uppgifter/login/submit.php
+++ Uppgifter/login/submit.php
@@ -6,8 +6,12 @@
 	$database = "register";
 	
 	$dbc = mysqli_connect($adress,$user,$password,$database);
-
+	
 	$username = $_POST['username']
 	$password = $_POST['pwd']
+	
+	$select = "SELECT username,password FROM data WHERE username='$username' AND password='$password';"
+	
+	
 	
 ?>

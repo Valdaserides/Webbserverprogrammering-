40b4e8aa4edeb8d8f28090a9ca53e01a1b5e693e
Valdaserides <max.valdaseridesolofsson.te16c@gmail.com>
2017-12-01 10:23:32 +0100

    lade till så att man måste ha fyllt ut allt i formuläret för att registrera sig

--- Uppgifter/register/submit.php
+++ Uppgifter/register/submit.php
@@ -7,25 +7,32 @@
 
 	$dbc = mysqli_connect($adress,$user,$password,$database);
 
-	$username = $_POST['username'];
-	$fullName = $_POST['name'];
-	$email = $_POST['email'];
-	$date = $_POST['date'];
-	$password = $_POST['pwd'];
-	
-	$insert = "INSERT INTO data (username, namn, email, date, password) VALUES ('$username', '$fullName', '$email', '$date', '$password');";
-	
-	if(mysqli_query($dbc, $insert)){
-		echo "Success!";
-		echo "<br>";
-		echo "Please go the <a href='../login/login.php'> login </a> page.";
+	if($_POST['username'] != NULL && $_POST['name'] != NULL && $_POST['email'] != NULL && $_POST['date'] != NULL && $_POST['password'] != NULL){
+		
+		$username = $_POST['username'];
+		$fullName = $_POST['name'];
+		$email = $_POST['email'];
+		$date = $_POST['date'];
+		$password = $_POST['pwd'];
+		
+		$insert = "INSERT INTO data (username, namn, email, date, password) VALUES ('$username', '$fullName', '$email', '$date', '$password');";
+		
+		if(mysqli_query($dbc, $insert)){
+			echo "Success!";
+			echo "<br>";
+			echo "Please go the <a href='../login/login.php'> login </a> page.";
+		}
+		else{
+			echo mysqli_error($dbc);
+			echo '<br>';
+			echo "Error!";
+		}
 	}
 	else{
-		echo mysqli_error($dbc);
-		echo '<br>';
-		echo "Error!";
+		
+		echo "Please fill out the form";
+		
 	}
-	
 	
 
 ?>

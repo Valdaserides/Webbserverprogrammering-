b3ab80dfa006599fa738f099dcc604c040851e25
Valdaserides <max.valdaseridesolofsson.te16c@gmail.com>
2017-12-08 10:10:22 +0100

    gjorde basic html och formulär

---
+++ test/index.php
@@ -0,0 +1,42 @@
+
+
+<!doctype html>
+<html>
+<head>
+
+	<link rel="stylesheet" href="css.css">
+
+	<title> hello </title>
+	
+<head>
+
+<body>
+
+	registrerings-formulär
+	<form method="POST" action="index.php">
+
+		Användarnamn: <input type="text" name="username">
+		<br>
+		E-mail: <input type="text" name="password">
+		<br>
+		Lösenord: <input type="password" name="pwd">
+		<br>
+		<input type="submit" name="submit">
+
+	</form>
+	
+	<br><br><br><br>
+	
+	login-formulär:
+	<form method="POST" action="index.php">
+
+		Användarnamn: <input type="text" name="username">
+		<br>
+		Lösenord: <input type="password" name="password">
+		<br>
+		<input type="submit" name="submit">
+
+	</form>
+
+</body>
+</html>

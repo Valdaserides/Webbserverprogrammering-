15c672bed33043b7765780fb01a124b38e89e696
Valdaserides <max.valdaseridesolofsson.te16c@gmail.com>
2018-01-16 15:49:50 +0100

    basic html,css,javascript kod

---
+++ javascript/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<html>
+<head>
+	
+	<meta charset="utf-8">
+	
+	<title>  </title>
+	
+	<style>
+
+	</style>
+	
+	<script>
+	
+	</script>
+
+</head>
+
+<body>
+
+
+
+
+</body>
+</html>
